00205ef65b3726624214154b416ec61699363bbd
rararaMiranda <miranda881@guru.smk.belajar.id>
2025-01-25 13:26:14 +0700

    feat: integrate to database se & create API Read

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 
 
 // Set port
-const port = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 4000;
 
 // Start server
 app.listen(port, () => {
